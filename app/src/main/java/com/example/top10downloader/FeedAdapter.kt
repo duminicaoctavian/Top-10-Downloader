be61ff53d85ec1971c2670799276be42b0de6d72
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-11 18:09:28 +0300

    ViewHolder Pattern

--- app/src/main/java/com/example/top10downloader/FeedAdapter.kt
+++ app/src/main/java/com/example/top10downloader/FeedAdapter.kt
@@ -8,8 +8,18 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 
-class FeedAdapter(context: Context, private val resource: Int, private val applications: List<FeedEntry>)
-    : ArrayAdapter<FeedEntry>(context, resource) {
+class ViewHolder(v: View) {
+    val tvName: TextView = v.findViewById(R.id.tvName)
+    val tvArtist: TextView = v.findViewById(R.id.tvArtist)
+    val tvSummary: TextView = v.findViewById(R.id.tvSummary)
+}
+
+
+class FeedAdapter(
+    context: Context,
+    private val resource: Int,
+    private val applications: List<FeedEntry>
+) : ArrayAdapter<FeedEntry>(context, resource) {
 
     private val TAG = "FeedAdapter"
     private val inflater = LayoutInflater.from(context)
@@ -24,24 +34,28 @@ class FeedAdapter(context: Context, private val resource: Int, private val appli
         Log.d(TAG, "getView() called")
 //        val view = inflater.inflate(resource, parent, false)
         val view: View
+        val viewHolder: ViewHolder
+
         if (convertView == null) {
             Log.d(TAG, "getView() called with null convertView")
             view = inflater.inflate(resource, parent, false)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
         } else {
             Log.d(TAG, "getView() provided a convertView")
             view = convertView
+            viewHolder = view.tag as ViewHolder
         }
 
-
-        val tvName: TextView = view.findViewById(R.id.tvName)
-        val tvArtist: TextView = view.findViewById(R.id.tvArtist)
-        val tvSummary:TextView = view.findViewById(R.id.tvSummary)
+//        val tvName: TextView = view.findViewById(R.id.tvName)
+//        val tvArtist: TextView = view.findViewById(R.id.tvArtist)
+//        val tvSummary:TextView = view.findViewById(R.id.tvSummary)
 
         val currentApp = applications[position]
 
-        tvName.text = currentApp.name
-        tvArtist.text = currentApp.artist
-        tvSummary.text = currentApp.summary
+        viewHolder.tvName.text = currentApp.name
+        viewHolder.tvArtist.text = currentApp.artist
+        viewHolder.tvSummary.text = currentApp.summary
 
         return view
     }
