fd66df10d5f0b3e54a67a4fb3b205dfd55f92d95
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-11 14:00:10 +0300

    Download the RSS Feed

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -4,6 +4,14 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,10 +38,35 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "onPostExecute: parameter is $result")
             }
 
-            override fun doInBackground(vararg params: String?): String {
-                Log.d(TAG, "doInBackground: starts with ${params[0]}")
-                return "doInBackground completed"
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${url[0]}") // Log.d does not remain in production builds
+                val rssFeed = downloadXML(url[0])
+                if (rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: Error downloading") // Log.e remains in production builds
+                }
+                return rssFeed
             }
+        }
+    }
+
+    private fun downloadXML(urlPath: String?): String {
+        var xmlResult = StringBuilder()
+
+        try {
+            val url = URL(urlPath)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            val response = connection.responseCode
+            Log.d(TAG, "downloadXML: The response code was $response")
+
+            val inputStream = connection.inputStream
+            val inputStreamReader = InputStreamReader(inputStream)
+            val reader = BufferedReader(inputStreamReader)
+        } catch (e: MalformedURLException) {
+            Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
+        } catch (e: IOException) {
+            Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
+        } catch (e: Exception) {
+            Log.e(TAG, "Unknown error: ${e.message}")
         }
     }
 }
