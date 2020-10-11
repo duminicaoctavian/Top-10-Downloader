e3d65797848252289b84fa65548b6ba33d58a993
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-11 13:16:11 +0300

    AsyncTask

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.top10downloader
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called")
+    }
+
+    private inner class DowloadData : AsyncTask<String, Void, String>() { // CTRL + O for all methods
+
+        private val TAG = "DownloadData"
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+        override fun doInBackground(vararg params: String?): String {
+            TODO("Not yet implemented")
+        }
     }
 }
