9e2fe03d6a3542fd20750a9adc9c703e7e85c9c2
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-11 15:27:52 +0300

    Android Permissions

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -78,6 +78,9 @@ class MainActivity : AppCompatActivity() {
                     Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
                 } catch (e: IOException) {
                     Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
+                } catch (e: SecurityException) {
+                    e.printStackTrace()
+                    Log.e(TAG, "downloadXML: Security exception. Needs permissions? ${e.message}")
                 } catch (e: Exception) {
                     Log.e(TAG, "Unknown error: ${e.message}")
                 }
